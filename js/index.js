7ef482834579d811eefbf34ce777d697d7090fde
Allan ROLLE <allan.rolle@laposte.net>
2019-01-08 16:43:56 +0100

    Add slider (section header)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,30 @@
 $(document).ready(function(){
-  //alert('ok');
+  
+
+
+  // Slider
+  $('.js-slider-button').on('click', function(){
+
+    // DOM elements
+    var $this = $(this);
+    var $currentSlide = $this.attr('data-number');
+    var $allButtons = $this.siblings();
+    var $sliderContent = $('.js-slider-img');
+    var $target = $sliderContent.css('background-image');
+    var $regex = /pic-\d+/;
+
+    // Add active class on first button
+    $('.js-slider-button').first().addClass('active');
+
+    // Change active class
+    $this.addClass('active');
+    $allButtons.removeClass('active');
+
+    // Change background image based on selected button
+    var $newImageName = 'pic-' + $currentSlide;
+    var $newImageUrl = $target.replace($regex, $newImageName);
+    $sliderContent.css('background-image', $newImageUrl);    
+  });
+
+
 });
